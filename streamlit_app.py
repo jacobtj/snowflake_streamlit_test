7ae7b35a8fe90731a14920cab1bb1511fa04db8e
jacobtj <66802943+jacobtj@users.noreply.github.com>
2023-08-11 14:44:44 -0400

    import test database into streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title("Test App")
 
@@ -7,3 +8,7 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.text('🥣 🥗 🐔 🥑🍞')
+
+streamlit.header('Fruits Database')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
