34b694e08a447a572c6ea50842821792a16b6920
jacobtj <66802943+jacobtj@users.noreply.github.com>
2023-08-11 14:42:37 -0400

    added test text to streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
-import streamlit;
+import streamlit
 
-streamlit.title("Test App");
+streamlit.title("Test App")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 🥗 🐔 🥑🍞')
