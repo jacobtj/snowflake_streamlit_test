b1de3a47bac0cc93bf74cf1d62330d9fcbd6358f
jacobtj <66802943+jacobtj@users.noreply.github.com>
2023-08-11 15:53:27 -0400

    added functionality allowing users to pick fruits in a list

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,12 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.text('🥣🥗🐔🥑🍞')
 
+#load dataset
 streamlit.header('Fruits Database')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+#add list to allow users to pick fruits
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+#display data
 streamlit.dataframe(my_fruit_list)
