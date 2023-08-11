674cd44d2412a00c50e73364815aed7a9f6cfff6
jacobtj <66802943+jacobtj@users.noreply.github.com>
2023-08-11 15:55:58 -0400

    Update streamlit_app.py
    Modified index of the database

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ streamlit.text('🥣🥗🐔🥑🍞')
 #load dataset
 streamlit.header('Fruits Database')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index("Fruit')
 
 #add list to allow users to pick fruits
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
