66b78261afcc2f4a36be1ff3d029ff86ed7556d1
jacobtj <66802943+jacobtj@users.noreply.github.com>
2023-08-17 15:55:59 -0400

    fixed typo on snowflake data portion

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,6 +66,6 @@ def insert_row_snowflake(new_fruit):
 add_my_fruit = streamlit.text_input('What fruit would you like the add')
 if streamlit.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-  fruityvice_data = get_fruityvice_data(add_my_fruit)
+  insert_result = insert_row_snowflake(add_my_fruit)
   my_cnx.close()
-  streamlit.text(fruityvice_data)
+  streamlit.text(insert_result)
