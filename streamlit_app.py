ab5a25edfa550dbd1c8433b7d1605b9886aaede6
jacobtj <66802943+jacobtj@users.noreply.github.com>
2023-08-17 15:07:33 -0400

    separated fruityvice api code into a separate function

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,16 +24,21 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #display data
 streamlit.dataframe(fruits_to_show)
 
-#display fruityvice api fruit data through user input
+#request data from fruityvice api call
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
+#display fruityvice api fruit data based on user input
 streamlit.header("Fruityvice Fruit Advice!")
 try: 
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+    fruityvice_data = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(fruityvice_data)
 except URLError as e:
   streamlit.error()
 
